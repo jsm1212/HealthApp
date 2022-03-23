c0a7bd47b83bf5d0e29a0d392d0c43d5c19d0a26
HONG <dud2680@gmail.com>
2022-03-23 17:31:28 +0900

    관리자 세부페이지 수정

--- HealthApp/app/src/main/java/com/example/healthapp/admin/AdminTrainerActivity.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/admin/AdminTrainerActivity.kt
@@ -2,6 +2,7 @@ package com.example.healthapp.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.healthapp.R
 
 class AdminTrainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
