82bcccb91187a2ea8776b6d9f030f7ac6573fbff
HONG <dud2680@gmail.com>
2022-03-23 17:33:13 +0900

    마이페이지, 관리자페이지 이동버튼 생성

--- HealthApp/app/src/main/java/com/example/healthapp/WorkActivity.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/WorkActivity.kt
@@ -4,19 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.healthapp.admin.AdminActivity
+import com.example.healthapp.mypage.MypageActivity
 
 class WorkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work)
 
-        val btn = findViewById<Button>(R.id.goToBbs)
+        val btn1 = findViewById<Button>(R.id.goToBbs)
+        val btn2 = findViewById<Button>(R.id.goToMypage)
+        val btn3 = findViewById<Button>(R.id.goToAdmin)
 
-        btn.setOnClickListener {
+        btn1.setOnClickListener {
             val intent = Intent(this, WorkBbsActivity::class.java)
             startActivity(intent)
         }
-
-
+        btn2.setOnClickListener {
+            val intent = Intent(this, MypageActivity::class.java)
+            startActivity(intent)
+        }
+        btn3.setOnClickListener {
+            val intent = Intent(this, AdminActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
