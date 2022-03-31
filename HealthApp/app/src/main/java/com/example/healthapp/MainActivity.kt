d3087f49b9714dd90163c09575ee350ae65bee19
baewoobin <b.w.bin77@gmail.com>
2022-03-31 13:06:52 +0900

    3/31

--- HealthApp/app/src/main/java/com/example/healthapp/MainActivity.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.healthapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import com.example.healthapp.login.LoginActivity
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,9 @@ class MainActivity : AppCompatActivity() {
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
         }
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("@@@@@@Hash", keyHash)
+
 
     }
 }
--- HealthApp/app/src/main/java/com/example/healthapp/fragment/CalendarFragment.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/fragment/CalendarFragment.kt
@@ -3,72 +3,72 @@ package com.example.healthapp.fragment
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import com.example.healthapp.CalendarDao
 import com.example.healthapp.CalendarDto
 import com.example.healthapp.R
 import com.example.healthapp.login.LoginMemberDao
-import java.util.*
+
 
 class CalendarFragment(val activity:Context) : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
-        val content = view.findViewById<EditText>(R.id.contextEditText)
-        val date = view.findViewById<TextView>(R.id.diaryTextView)
-        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
-        val contentView = view.findViewById<TextView>(R.id.contentView)
-        val caltextView = view.findViewById<TextView>(R.id.caltextView)
-        val contextEditText = view.findViewById<EditText>(R.id.contextEditText)
-        val saveBtn = view.findViewById<Button>(R.id.save_Btn)
-        val chrBtn = view.findViewById<Button>(R.id.cha_Btn)
-        val delBtn = view.findViewById<Button>(R.id.del_Btn)
-        contentView.visibility = View.VISIBLE
-        chrBtn.visibility = View.VISIBLE
-        delBtn.visibility = View.VISIBLE
-        caltextView.visibility = View.VISIBLE
-        contextEditText.visibility = View.VISIBLE
-        saveBtn.visibility = View.VISIBLE
+        val content = view.findViewById<EditText>(R.id.contextEditText) //내용 Edit
+        val date = view.findViewById<TextView>(R.id.diaryTextView) //날짜 TEXT
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView) //달력
+        val contentView = view.findViewById<TextView>(R.id.contentView) //내용 TEXT
+        val caltextView = view.findViewById<TextView>(R.id.caltextView) //안내 TEXT
+        val saveBtn = view.findViewById<Button>(R.id.save_Btn) //저장 버튼
+        val chrBtn = view.findViewById<Button>(R.id.cha_Btn) //수정 버튼
+        val delBtn = view.findViewById<Button>(R.id.del_Btn) //삭제 버튼
 
+        //감추기,보이기
+        contentView.visibility = View.INVISIBLE
+        chrBtn.visibility = View.INVISIBLE
+        delBtn.visibility = View.INVISIBLE
+        caltextView.visibility = View.INVISIBLE
+        saveBtn.visibility = View.INVISIBLE
+        content.visibility = View.INVISIBLE
+
+        //유저 아이디
         val currentUserId = LoginMemberDao.user?.id
 
         content.setText("")
+
+        // 달력 날짜 선택
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            // 달력 날짜가 선택되면
-            date.visibility = View.VISIBLE // 해당 날짜가 뜨는 textView가 Visible
+            date.visibility = View.VISIBLE
             date.text = String.format("%d/%d/%d", year, month + 1, dayOfMonth)
             System.out.println("@@@@@@@@!!!!!"+date.text.toString())
-            // var selectDate = "${year}/${month+1}/${dayOfMonth}"
             // 날짜를 보여주는 텍스트에 해당 날짜를 넣는다.
-            //title.setText("")
+
             content.setText("") // EditText에 공백값 넣기
             val dto = CalendarDao.getInstance().searchCalendar(CalendarDto(0,"","","",0,currentUserId.toString(),date.text.toString()))
-            if(dto != null){
-                contentView.text=""
-                contentView.text="내용:${dto.content}"
-                contentView.visibility = View.VISIBLE
-                chrBtn.visibility = View.VISIBLE
-                delBtn.visibility = View.VISIBLE
-                caltextView.visibility = View.VISIBLE
-                contextEditText.visibility = View.VISIBLE
-                saveBtn.visibility = View.INVISIBLE
-            }else{
-                contentView.text=""
-                caltextView.visibility = View.VISIBLE
-                contextEditText.visibility = View.VISIBLE
-                saveBtn.visibility = View.VISIBLE
-                chrBtn.visibility = View.INVISIBLE
-                delBtn.visibility = View.INVISIBLE
-            }
-     }
+                if(dto != null){
+                    contentView.text=""
+                    contentView.text="내용:${dto.content}"
+                    contentView.visibility = View.VISIBLE
+                    chrBtn.visibility = View.VISIBLE
+                    delBtn.visibility = View.VISIBLE
+                    caltextView.visibility = View.VISIBLE
+                    saveBtn.visibility = View.INVISIBLE
+                    content.visibility = View.VISIBLE
+                }else{
+                    contentView.text=""
+                    caltextView.visibility = View.VISIBLE
+                    contentView.visibility = View.VISIBLE
+                    saveBtn.visibility = View.VISIBLE
+                    chrBtn.visibility = View.INVISIBLE
+                    delBtn.visibility = View.INVISIBLE
+                    content.visibility = View.VISIBLE
+                }
+        }
 
         delBtn.setOnClickListener {
             val msg = CalendarDao.getInstance().deleteCalendar(CalendarDto(0,"","","",0,currentUserId,date.text.toString()))
@@ -79,32 +79,35 @@ class CalendarFragment(val activity:Context) : Fragment() {
                     chrBtn.visibility = View.INVISIBLE
                     delBtn.visibility = View.INVISIBLE
                     caltextView.visibility = View.INVISIBLE
-                    contextEditText.visibility = View.INVISIBLE
                     saveBtn.visibility = View.INVISIBLE
+                    content.visibility = View.INVISIBLE
                 }else{
-                    Log.e("@@@@@삭제에러",msg)
+                    Toast.makeText(activity, "제거하지 못했습니다.", Toast.LENGTH_LONG).show()
                 }
             }
         }
 
         saveBtn.setOnClickListener{
-            Log.d("@@@@@@", "aaaaaaaaaa!!!!!!!!!!!!!")
-           // val t = title.text.toString()
-            //Log.d("@@@@@@",t)
             val c = content.text.toString()
-            Log.d("@@@@@@",c)
             val msg = CalendarDao.getInstance().writeCalendar_M(CalendarDto(0,"",c,"",0,currentUserId,date.text.toString()))
             if( msg != null) {
-                Log.d("@@@@@에러", msg!!)
                 if(msg == "YES") {
                     Toast.makeText(activity, "저장되었습니다.", Toast.LENGTH_LONG).show()
+                    contentView.visibility = View.INVISIBLE
+                    chrBtn.visibility = View.INVISIBLE
+                    delBtn.visibility = View.INVISIBLE
+                    caltextView.visibility = View.INVISIBLE
+                    saveBtn.visibility = View.INVISIBLE
+                    content.visibility = View.INVISIBLE
+                }else{
+                    Toast.makeText(activity, "저장하지 못했습니다.", Toast.LENGTH_LONG).show()
                 }
             }
         }
+
         chrBtn.setOnClickListener {
             val c = content.text.toString()
             caltextView.visibility = View.VISIBLE
-            contextEditText.visibility = View.VISIBLE
             saveBtn.visibility = View.VISIBLE
             val msg = CalendarDao.getInstance().updateCalendar(CalendarDto(0,"",c,"",0,currentUserId,date.text.toString()))
             if( msg != null) {
@@ -114,15 +117,16 @@ class CalendarFragment(val activity:Context) : Fragment() {
                     chrBtn.visibility = View.INVISIBLE
                     delBtn.visibility = View.INVISIBLE
                     caltextView.visibility = View.INVISIBLE
-                    contextEditText.visibility = View.INVISIBLE
                     saveBtn.visibility = View.INVISIBLE
+                    content.visibility = View.INVISIBLE
+                }
+                else{
+                    Toast.makeText(activity, "수정하지 못했습니다.", Toast.LENGTH_LONG).show()
                 }
             }
         }
         return view
-
     }
-
 }
 
 
