f5fe88b3368599cefb0e4644fc51e38d2310350a
Noh-jisu <nojisu123@naver.com>
2022-04-11 16:04:06 +0900

    액티비티 매개변수 테스트

--- HealthApp/app/src/main/java/com/example/healthapp/bbs/WorkActivity.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/bbs/WorkActivity.kt
@@ -11,14 +11,29 @@ import com.example.healthapp.fragment.MypageFragment
 import com.example.healthapp.fragment.WorklistFragment
 
 class WorkActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_work)
 
         val fm = supportFragmentManager
+
         val fragmentTransaction = fm.beginTransaction()
         fragmentTransaction.add(R.id.frView, WorklistFragment(this, applicationContext))
         fragmentTransaction.commit()
+
+//        if(index == null || index == 1){
+//            fragmentTransaction.add(R.id.frView, WorklistFragment(WorkActivity(1), applicationContext))
+//        }else if(index != null && index == 2){
+//            fragmentTransaction.add(R.id.frView, BbsFragment(WorkActivity(2)))
+//        }else if(index != null && index == 3){
+//            fragmentTransaction.add(R.id.frView, CalendarFragment(WorkActivity(3)))
+//        }else if(index != null && index == 4){
+//            fragmentTransaction.add(R.id.frView, MypageFragment(WorkActivity(4)))
+//        }else{
+//            fragmentTransaction.add(R.id.frView, WorklistFragment(WorkActivity(1), applicationContext))
+//        }
+//        fragmentTransaction.commit()
     }
 
     override fun onClick(view: View?) {
