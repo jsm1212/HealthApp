501dddbd06bdb22b6e592c2bd4861e480fa8bbf7
Noh-jisu <nojisu123@naver.com>
2022-04-17 17:04:58 +0900

    기능구현 최종

--- HealthApp/app/src/main/java/com/example/healthapp/fragment/WorklistFragment.kt
+++ HealthApp/app/src/main/java/com/example/healthapp/fragment/WorklistFragment.kt
@@ -1,8 +1,8 @@
 package com.example.healthapp.fragment
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,41 +12,40 @@ import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.healthapp.OnMyKeyDown
 import com.example.healthapp.R
 import com.example.healthapp.work.WorkAdapter
-import com.example.healthapp.work.WorkVo
+import com.example.healthapp.work.WorkDao
+import com.example.healthapp.work.WorkDto
 
 class WorklistFragment(val activity:Context, val applicationContext: Context) : Fragment() {
 
-    var testList = arrayListOf<WorkVo>(
-        WorkVo("운동이름1", "설명1", "이미지1", 0),
-        WorkVo("운동이름2", "설명2", "이미지2", 1),
-        WorkVo("운동이름3", "설명3", "이미지3", 2),
-        WorkVo("운동이름4", "설명4", "이미지4", 3)
-    )
+    /*var testList = arrayListOf<WorkDto>(
+        WorkDto(0,"어깨 운동1", "어깨 운동1", "이미지1", 0,0),
+        WorkDto(1,"가슴 운동1", "가슴 운동1", "이미지2", 1,0),
+        WorkDto(2,"복부 운동1", "복부 운동1", "이미지3", 2,0),
+        WorkDto(3,"하체 운동1", "하체 운동1", "이미지4", 3,0),
+        WorkDto(4,"어깨 운동2", "어깨 운동2", "이미지1", 0,0),
+        WorkDto(5,"가슴 운동2", "가슴 운동2", "이미지2", 1,0),
+        WorkDto(6,"복부 운동2", "복부 운동2", "이미지3", 2,0),
+        WorkDto(7,"하체 운동2", "하체 운동2", "이미지4", 3,0)
+    )*/
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_worklist, container, false)
 
-        // 뒤로가기 버튼 2번 클릭시 어플 종료
-        view.isFocusableInTouchMode = true
-        view.requestFocus()
-        view.setOnKeyListener(OnMyKeyDown(activity as Activity))
-
         val reView = view.findViewById<RecyclerView>(R.id.rv)
 
         val workSpinner = view.findViewById<Spinner>(R.id.workSpinner)
-        val workcategory = listOf("전체", "어깨", "가슴", "배", "하체")
+        val workcategory = listOf("전체", "어깨","가슴", "복부", "하체", "팔")
 
         workSpinner.adapter = ArrayAdapter<String>(activity, R.layout.item_spinner, workcategory)
-
+        val worklist=WorkDao.getInstance().getWorkList()
         workSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
-                val mAdapter = WorkAdapter(activity, testList)
+                val mAdapter = WorkAdapter(activity, worklist!!)
                 mAdapter.filter.filter("$pos")
-                reView.adapter = WorkAdapter(applicationContext, testList)
+                reView.adapter = WorkAdapter(applicationContext, worklist)
                 println("~~~~~~~~~~~~worklistActivity mAdapter~~~~")
                 reView.adapter=mAdapter
                 println("~~~~~~~~~~~~worklistActivity mAdapterin~~~~")
